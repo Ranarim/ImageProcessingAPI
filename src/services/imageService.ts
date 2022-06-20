d431cb3ed7ca2fa7bf0188e36782e14f2ed61b9f
Johannes <74767916+Ranarim@users.noreply.github.com>
2022-06-20 16:35:05 +0200

    updating the functioning service

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -1,26 +1,26 @@
 /* THIS COMPONENT INTERACTS WITH THE DATA BASE */
 import { Request, Response } from 'express'
 import * as fs from 'fs'
-import sharp from "sharp"
+import sharp from 'sharp'
 export class imageServices {
 	changeAndDisplayImage(req: Request, res: Response) {
 		// define the original image path
-		let imagePath = './src/database/' + req.params.pathname
+		let imagePath = './src/database/' + req.params.imageName
 
 		if (!fs.existsSync(imagePath)) {
 			imagePath = './src/database/image-not-found.jpg'
 		}
 
-		const width = req.query.width ? parseInt(req.query.width) : 500
-		const height = req.query.height ? parseInt(req.query.height) : 400
+		const width = req.query.width ? parseInt(req.query.width.toString()) : 500
+		const height = req.query.height ? parseInt(req.query.height.toString()) : 400
 
 		const stream = fs.createReadStream(imagePath)
 
 		const transform = sharp().resize(width, height)
 
-		stream.pipe(transform).on(‘error’, (e) => {}).pipe(res);
+		stream.pipe(transform).pipe(res)
 
-		return stream;
+		return stream
 	}
 }
 
