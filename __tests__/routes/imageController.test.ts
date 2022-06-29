e44ea614790bb8edb6958d39e488edfd6c023c7c
Johannes <74767916+Ranarim@users.noreply.github.com>
2022-06-29 16:52:54 +0200

    Ready for submit

--- __tests__/routes/imageController.test.ts
+++ __tests__/routes/imageController.test.ts
@@ -1,14 +1,17 @@
 import app from '../../src/index'
 import request from 'supertest'
 
-describe('Testing Controller and fs functionality', (): void => {
-	test('check if the status is 400 if image doesnt exist', () => {
-		request(app).get('/api/images/krxln').expect(404)
+describe('Testing Controller and fs functionality', () => {
+	test('check if the status is 400 if image doesnt exist', async (): Promise<void> => {
+		const data = await request(app).get('/api/images/krxln')
+		expect(data.status).toBe(404)
 	})
-	test('check if the status is 200 if the thumb is created', () => {
-		request(app).get('/api/images/klettern?width=500&height=500').expect(200)
+	test('check if the status is 200 if the thumb is created', async (): Promise<void> => {
+		const data = await request(app).get('/api/images/klettern?width=888&height=888')
+		expect(data.status).toBe(200)
 	})
-	test('check if the status is 202 if the thumb already exists', () => {
-		request(app).get('/api/images/klettern?width=500&height=500').expect(200)
+	test('check if the status is 202 if the thumb already exists', async (): Promise<void> => {
+		const data = await request(app).get('/api/images/klettern?width=500&height=500')
+		expect(data.status).toBe(202)
 	})
 })
