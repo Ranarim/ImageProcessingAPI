01c0a7833cd707ed09b80f98ee48ee5896631aff
Johannes <74767916+Ranarim@users.noreply.github.com>
2022-06-29 20:11:42 +0200

    delete[...]: Getting rid of the built dir

--- built/src/routes/imageRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var getImageController_1 = require("../controllers/getImageController");
-var router = (0, express_1.Router)();
-router.get('/:imageName', getImageController_1.getImage);
-exports.default = router;
